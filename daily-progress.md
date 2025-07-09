35c627c27cdb7cbb7965fff72a14a925065cd407
root <root@vmi2700459.contaboserver.net>
2025-07-08 23:36:57 -0700

    demo yfinance

---
+++ daily-progress.md
@@ -0,0 +1,3 @@
+## 2025-7-9
+- 开发yfinance最小功能集
+- 准备先实现个最小demo，如data-source文件夹中yfinance获取数据，处理股票，调用aiagent，生产tutorial-beginner里的一个示例报告。
---
+++ myfinrobot/yfinance.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File: yfinance.py
+Created Time: 2025-07-09 10:48:52
+Author: Coin Lau (jinxinliu@gmail.com)
+Description: yfinance API for financial data retrieval
+"""
+
+import sys
+def main():
+    #print("Python executable:", sys.executable)  # 应显示 fintech 环境的路径
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
